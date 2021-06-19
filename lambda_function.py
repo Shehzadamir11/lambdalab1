3e7163977998e5b0b2b368c04ad0ed14d0dd24d3
Shehzadamir11 <65251670+Shehzadamir11@users.noreply.github.com>
2021-06-19 14:08:13 +1000

    second

--- lambda_function.py
+++ lambda_function.py
@@ -15,6 +15,6 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Serverless API '),
+        'body': json.dumps('Hello from Serverless API via github '),
         #'body': json.dumps(event)
     }
