f05173bd5fd3eae1c50c70efd7c93084483aee14
Shehzadamir11 <65251670+Shehzadamir11@users.noreply.github.com>
2021-06-20 18:16:38 +1000

    Shehzad

--- index.py
+++ index.py
@@ -15,6 +15,6 @@ def handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Your Hittime Entry is successfully written to Dynamo DB Version 2 '),
+        'body': json.dumps('Your Hittime Entry is successfully written to Dynamo DB Version 3 with Hassan '),
         #'body': json.dumps(event)
     }
